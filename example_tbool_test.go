9bfa4be20b5a52e79a25c01c48f6236d1273f545
facingfrost <wanglinhanlydia@163.com>
2024-08-20 10:11:09 +0200

    modify testable examples

--- example_tbool_test.go
+++ example_tbool_test.go
@@ -1,25 +1,23 @@
-package gomeos_test
+package gomeos
 
 import (
 	"fmt"
 	"testing"
-
-	gomeos "github.com/MobilityDB/GoMEOS"
 )
 
 func ExampleTBoolOut() {
-	gomeos.MeosInitialize("UTC")
-	g_is := gomeos.NewTBoolInst("TRUE@2022-10-01")
-	fmt.Println(gomeos.TBoolOut(g_is))
+	MeosInitialize("UTC")
+	g_is := NewTBoolInst("TRUE@2022-10-01")
+	fmt.Println(TBoolOut(g_is))
 	// Output:
 	// t@2022-10-01 00:00:00+00
 }
 
 func TestTPointToSTBox(t *testing.T) {
-	gomeos.MeosInitialize("UTC")
-	tg := gomeos.TGeomPointIn("{POINT(1 1)@2022-10-01 00:00:00+00, POINT(2 2)@2022-10-02 00:00:00+00}", &gomeos.TGeomPointSeq{})
-	stbox := gomeos.TPointToSTBox(tg)
-	fmt.Println(gomeos.STBoxOut(stbox, 10))
+	MeosInitialize("UTC")
+	tg := TGeomPointIn("{POINT(1 1)@2022-10-01 00:00:00+00, POINT(2 2)@2022-10-02 00:00:00+00}", &TGeomPointSeq{})
+	stbox := TPointToSTBox(tg)
+	fmt.Println(STBoxOut(stbox, 10))
 	// Output:
 	// STBOX XT(((1,1),(2,2)),[2022-10-01 00:00:00+00, 2022-10-02 00:00:00+00])
 }
